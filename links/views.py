e2592ff89e9630ea7c533c18589194f4d5c0ba8f
Pablo SEMINARIO <pabluk@gmail.com>
2013-12-01 17:20:48 +0100

    Index view for links

--- links/views.py
+++ links/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from links.models import Linkshelf
+
+
+def index(request):
+    linkshelf = Linkshelf.objects.latest('id')
+    colors = ['#66D596', '#66A7D5', '#FF566A']
+    context = {'linkshelf': linkshelf, 'colors': colors}
+    return render(request, 'links/index.html', context)
