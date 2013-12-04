88b9bd5378ac1b688f20f5d69fb8e6725bfcf901
Pablo SEMINARIO <pabluk@gmail.com>
2013-12-05 00:02:39 +0100

    Configuration for Heroku

--- bokmarken/settings.py
+++ bokmarken/settings.py
@@ -24,7 +24,7 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 BROWSERID_AUDIENCES = ['http://localhost:8000']
 
@@ -88,6 +88,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
@@ -109,3 +110,10 @@ LOGIN_REDIRECT_URL_FAILURE = '/'
 
 # Path to redirect to on logout.
 LOGOUT_REDIRECT_URL = '/'
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
