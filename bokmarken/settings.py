0712445bdc088547a122d6b413b5af0d31dafe8f
Pablo SEMINARIO <pabluk@gmail.com>
2013-12-06 23:58:57 +0100

    Removed Heroku's specific settings

--- bokmarken/settings.py
+++ bokmarken/settings.py
@@ -24,7 +24,7 @@ DEBUG = os.getenv('DEBUG', False)
 
 TEMPLATE_DEBUG = DEBUG
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 SITE_ID = 1
 
@@ -87,13 +87,6 @@ TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'bokmarken', 'templates')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 LOGIN_REDIRECT_URL = '/'
 
