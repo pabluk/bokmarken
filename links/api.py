8a233cd7199df34427e759f8ead36f98e09165c1
Pablo SEMINARIO <pabluk@gmail.com>
2013-12-07 11:34:17 +0100

    Some changes to api.py

--- links/api.py
+++ links/api.py
@@ -10,9 +10,17 @@ class LinkResource(ModelResource):
     class Meta:
         queryset = Link.objects.all()
         resource_name = 'link'
-        excludes = ['is_updated', 'is_public']
+        #excludes = ['is_updated', 'is_public']
+        fields = ['url']
+        allow_methods = ['get', 'post']
         authentication = ApiKeyAuthentication()
         authorization = Authorization()
+
+    def obj_create(self, bundle, **kwargs):
+        return super(LinkResource, self).obj_create(bundle, user=bundle.request.user)
+
+    def apply_authorization_limits(self, request, object_list):
+        return object_list.filter(user=request.user)
 
     def get_object_list(self, request):
         return super(LinkResource, self).get_object_list(request).filter(user=request.user)
