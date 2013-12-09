68e878bb3a4cd47968b48d9a8c814747fb630225
Pablo SEMINARIO <pabluk@gmail.com>
2013-12-09 23:29:17 +0100

    Added the about and the API pages

--- bokmarken/urls.py
+++ bokmarken/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 admin.autodiscover()
 
 from tastypie.api import Api
@@ -17,6 +18,9 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(v1_api.urls)),
     (r'^accounts/', include('registration.backends.default.urls')),
+
+    url(r'^api/$', TemplateView.as_view(template_name='api.html'), name='api'),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
 
     url(r'^', include('links.urls')),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
