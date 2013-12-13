af843072f4566ab59fe1987cf5c917a886351468
Pablo SEMINARIO <pabluk@gmail.com>
2013-12-13 08:43:50 +0100

    Updated tests for links app

--- links/tests.py
+++ links/tests.py
@@ -1,42 +1,33 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 
-from links.models import Link, Linkshelf
+from links.models import Link
 
 class LinkTestCase(TestCase):
     def setUp(self):
-        user = User.objects.create_user('test_user', 'test@example.com', 'test_pass')
-        self.linkshelf = user.linkshelf_set.latest('id')
+        self.user = User.objects.create_user('test_user', 'test@example.com', 'test_pass')
 
     def test_domain(self):
         url = 'http://www.python.org/about/'
-        link = Link.objects.create(url=url, linkshelf=self.linkshelf)
+        link = Link.objects.create(url=url, is_update=True, user=self.user)
         self.assertEqual(link.domain(), 'www.python.org')
 
     def test_domain_complete_url(self):
         url = 'http://www.example.com:8080/path1/path2?queryparams=value#hashfragment'
-        link = Link.objects.create(url=url, linkshelf=self.linkshelf)
+        link = Link.objects.create(url=url, is_update=True, user=self.user)
         self.assertEqual(link.domain(), 'www.example.com')
 
     def test_simple_url(self):
         url = 'http://www.python.org/about/'
-        link = Link.objects.create(url=url, linkshelf=self.linkshelf)
+        link = Link.objects.create(url=url, is_update=True, user=self.user)
         self.assertEqual(link.simple_url(), 'www.python.org/about/')
 
     def test_simple_url_domain_end_slash(self):
         url = 'http://www.python.org/'
-        link = Link.objects.create(url=url, linkshelf=self.linkshelf)
+        link = Link.objects.create(url=url, is_update=True, user=self.user)
         self.assertEqual(link.simple_url(), 'www.python.org')
 
     def test_simple_url_domain_complete_url(self):
         url = 'http://example.com:8080/path?q=v#frag'
-        link = Link.objects.create(url=url, linkshelf=self.linkshelf)
+        link = Link.objects.create(url=url, is_update=True, user=self.user)
         self.assertEqual(link.simple_url(), 'example.com:8080/path?q=v#frag')
-
-
-class LinkshelfTestCase(TestCase):
-    def test_linkshelf_creation(self):
-        user_name = 'test_user'
-        user = User.objects.create_user(user_name, '%s@example.com' % user_name, 'test_pass')
-        linkshelf = Linkshelf.objects.get(name=user_name)
-        self.assertEqual(user_name, linkshelf.name)
