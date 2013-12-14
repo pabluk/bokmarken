874672f998b7a8b299044def9e6c38952ed0f01b
Pablo SEMINARIO <pabluk@gmail.com>
2013-12-14 11:47:25 +0100

    Implemented search form

--- links/forms.py
+++ links/forms.py
@@ -1,9 +1,14 @@
-from django.forms import ModelForm
+from django import forms
 
 from links.models import Link
 
 
-class LinkForm(ModelForm):
+class LinkForm(forms.ModelForm):
     class Meta:
         model = Link
         fields = ['url', 'is_public', 'auto_update']
+
+
+class LinkSearchForm(forms.Form):
+    q = forms.CharField(max_length=1024, required=True,
+                        label=u'Search', help_text=u'Search by URLs and title')
