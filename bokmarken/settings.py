6543555ba92913fee95e5a9ea79945fd5d99f500
Pablo Seminario <pablo@seminar.io>
2023-07-24 23:45:13 +0200

    Use safe default settings
    It removes a default `SECRET_KEY` and set `DEBUG` to False

--- bokmarken/settings.py
+++ bokmarken/settings.py
@@ -17,10 +17,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_y$%rthyh-cs-=gvhf34%70l_)2wn846_ne3i(b9#avbkj+78i'
+# See https://docs.djangoproject.com/en/4.2/ref/settings/#secret-key
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = DEBUG
 
